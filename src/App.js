62f440b9e3a65ebc69feea2afbf8f5b928782782
Parbati <paru@192-168-1-104.tpgi.com.au>
2021-09-26 11:45:23 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      starting admin cms 
+      <Button variant ='success'>test button</Button>
+      <i className="fab fa-500px"></i>
+
+    </div>
+  );
+}
+
+export default App;
